a24c94b0d77a32394e16300527f19dacecd9a606
Grigory Shipunov <blame@oxapentane.com>
2022-12-30 18:20:16 +0100

    cachix is still at old address

--- nixos-modules/TLMS/binary-cache.nix
+++ nixos-modules/TLMS/binary-cache.nix
@@ -11,11 +11,11 @@
   config = lib.mkIf config.TLMS.useBinaryCache {
     nix = {
       binaryCaches = [
-        "https://TLMS.cachix.org"
+        "https://dump-dvb.cachix.org"
         "https://hydra.hq.c3d2.de"
       ];
       binaryCachePublicKeys = [
-        "TLMS.cachix.org-1:+Dq7gqpQG4YlLA2X3xJsG1v3BrlUGGpVtUKWk0dTyUU="
+        "dump-dvb.cachix.org-1:+Dq7gqpQG4YlLA2X3xJsG1v3BrlUGGpVtUKWk0dTyUU="
         "nix-serve.hq.c3d2.de:KZRGGnwOYzys6pxgM8jlur36RmkJQ/y8y62e52fj1ps="
       ];
     };
