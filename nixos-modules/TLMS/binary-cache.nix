13e0234e5cb67e4795ecea4529e9546ae90d1520
Grigory Shipunov <blame@oxapentane.com>
2022-12-31 16:18:24 +0100

    update legacy options

--- nixos-modules/TLMS/binary-cache.nix
+++ nixos-modules/TLMS/binary-cache.nix
@@ -9,12 +9,12 @@
   };
 
   config = lib.mkIf config.TLMS.useBinaryCache {
-    nix = {
-      binaryCaches = [
+    nix.settings = {
+      substituters = [
         "https://dump-dvb.cachix.org"
         "https://hydra.hq.c3d2.de"
       ];
-      binaryCachePublicKeys = [
+      trusted-public-keys = [
         "dump-dvb.cachix.org-1:+Dq7gqpQG4YlLA2X3xJsG1v3BrlUGGpVtUKWk0dTyUU="
         "nix-serve.hq.c3d2.de:KZRGGnwOYzys6pxgM8jlur36RmkJQ/y8y62e52fj1ps="
       ];
